8ca6e0c58a23cd757279eac74e6bfc9f07fdc214
mhmadamrii <interceptorghost4@gmail.com>
2025-04-29 23:48:40 +0700

    .prettierrc

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.get("/:name", (c) => {
+  console.log(c);
+  return c.text(`Hello ${c.req.param("name")}!`);
+});
+
+app.get("/dummy", (c) => {
+  return c.json({ message: "Hello World!" });
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);
